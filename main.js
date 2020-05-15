02a38729291108f6df7396514e752b4e4450af92
Alba-Tros <samipcpoolberlin@yahoo.fr>
2020-05-15 22:46:44 +0200

    Fix collapsing hamburger menu

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const menuBtn = document.querySelector(".hamburger");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
+    if (!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    }
+});
+
+const menuItem = document.querySelectorAll(".hamburger-item a");
+menuItem.forEach(hamitem => {
+    hamitem.addEventListener("click", () => {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    });
 });
